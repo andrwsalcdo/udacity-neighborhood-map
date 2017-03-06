a84dc804c3b75947b03e4bb497b0bc5a351ae649
Andrew Salcedo <Andrew Salcedo>
2017-03-06 16:12:48 -0500

    add 1 venue marker with infowindow. basic.

--- js/app.js
+++ js/app.js
@@ -9,11 +9,33 @@ var map; // declares a global map variable
   Start here! initMap() is called when page is loaded.
 */
 function initMap() {
-  //create a new map-- only center & zoom required
-  map = new google.maps.Map(document.getElementById('map'), {
-    center: {lat: 28.424653  , lng: -81.469516 },
+  //location of Star Wars Celebration 2017, & 'center' & 1 marker of map
+  var starWarsVenue = {lat: 28.424653, lng: -81.469516};
+  //properties of Map
+  var mapOptions = {
+    center: starWarsVenue,
     zoom: 13
+  }
+  //create a new map-- only center & zoom required
+  map = new google.maps.Map(document.getElementById('map'), mapOptions);
+
+  venueMarker = new google.maps.Marker({
+    map: map,
+    animation: google.maps.Animation.DROP,
+    position: starWarsVenue,
+    title: "Star Wars Celebration 2017!"
   });
+
+  var infowindow = new google.maps.InfoWindow({
+    content: venueMarker.title
+  });
+
+  venueMarker.addListener('click', function() {
+    infowindow.open(map,venueMarker);
+  });
+
+
+
 }
 
 /*
